64d2caf3458666a188ec06b5d7d9d341dd15d8ce
CarlosReyesStoneham <cer26@duke.edu>
2014-01-18 18:42:29 -0500

    test push

---
+++ RISC/test.cpp
@@ -0,0 +1,13 @@
+//
+//  test.cpp
+//  RISC
+//
+//  Created by Carlos Reyes on 1/18/14.
+//  Copyright (c) 2014 Carlos Reyes. All rights reserved.
+//
+
+#include "test.h"
+
+int main(){
+    
+}
---
+++ RISC/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  RISC
+//
+//  Created by Carlos Reyes on 1/18/14.
+//  Copyright (c) 2014 Carlos Reyes. All rights reserved.
+//
+
+#ifndef __RISC__test__
+#define __RISC__test__
+
+#include <iostream>
+
+#endif /* defined(__RISC__test__) */
