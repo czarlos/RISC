f6faeb52197f6f92afb675d3e587828ce54920ba
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-01-18 18:44:52 -0500

    added test 2

---
+++ RISC/Test 2.cpp
@@ -0,0 +1,9 @@
+//
+//  Test 2.cpp
+//  RISC
+//
+//  Created by Wei Chen on 1/18/14.
+//  Copyright (c) 2014 Wei Chen. All rights reserved.
+//
+
+#include "Test 2.h"
---
+++ RISC/Test 2.h
@@ -0,0 +1,14 @@
+//
+//  Test 2.h
+//  RISC
+//
+//  Created by Wei Chen on 1/18/14.
+//  Copyright (c) 2014 Wei Chen. All rights reserved.
+//
+
+#ifndef __RISC__Test_2__
+#define __RISC__Test_2__
+
+#include <iostream>
+
+#endif /* defined(__RISC__Test_2__) */
