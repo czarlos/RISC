353e11976275ec7c32724c7300bd0d9e65ade9bf
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-01-18 18:32:30 -0500

    Initial Commit

---
+++ RISC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RISC
+//
+//  Created by Wei Chen on 1/18/14.
+//  Copyright (c) 2014 Wei Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
