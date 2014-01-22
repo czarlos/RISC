ae7a5f8cabe82bb8be0b14b66ddba33baa84525c
CarlosReyesStoneham <cer26@duke.edu>
2014-01-22 14:25:59 -0500

    declarations in header files

---
+++ RISC/Location.h
@@ -0,0 +1,19 @@
+//
+//  Location.h
+//  RISC
+//
+//  Created by Carlos Reyes on 1/18/14.
+//  Copyright (c) 2014 Carlos Reyes. All rights reserved.
+//
+
+#ifndef __RISC__Location__
+#define __RISC__Location__
+
+#include <iostream>
+
+class Location {
+public:
+	Location(){}
+};
+
+#endif /* defined(__RISC__Location__) */
