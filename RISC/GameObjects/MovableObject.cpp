e1a022b99e1c00b7e1f4de7b7fbb2062f9374964
CarlosReyesStoneham <cer26@duke.edu>
2014-02-02 22:10:03 -0500

    added sub directories in RISC

---
+++ RISC/GameObjects/MovableObject.cpp
@@ -0,0 +1,9 @@
+//
+//  MovableObject.cpp
+//  RISC
+//
+//  Created by Carlos Reyes on 1/22/14.
+//  Copyright (c) 2014 Carlos Reyes. All rights reserved.
+//
+
+#include "MovableObject.h"
---
+++ RISC/GameObjects/StaticObject.cpp
@@ -0,0 +1,9 @@
+//
+//  StaticObject.cpp
+//  RISC
+//
+//  Created by Carlos Reyes on 1/22/14.
+//  Copyright (c) 2014 Carlos Reyes. All rights reserved.
+//
+
+#include "StaticObject.h"
