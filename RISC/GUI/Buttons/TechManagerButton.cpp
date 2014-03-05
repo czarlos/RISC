606422351f32e736c3190f8cda47dd2dd62cb62e
Wei Chen <wei.zheng.chen@duke.edu>
2014-03-05 00:43:31 -0500

    change texttodisplay to not allocate mem on the heap

--- RISC/GUI/Buttons/TechManagerButton.cpp
+++ RISC/GUI/Buttons/TechManagerButton.cpp
@@ -16,19 +16,6 @@ void TechManagerButton::onMouseClick(){
 		UnitPainter* background= new UnitPainter(&techManagerInfo);
 		techManagerInfo.setPosition(Vector2i(0, 0));
 
-
-		sf::Text* level = new sf::Text();
-		PopupWindows::makeText(level, "Current Technology Level: ", font, 10, 10, 14);
-
-		sf::Text* levelText = new sf::Text();
-		PopupWindows::makeText(levelText, std::to_string(this->techManager->getCurrentLevel()), font, 10, 40, 14);
-
-		sf::Text* upgrade = new sf::Text();
-		PopupWindows::makeText(upgrade, "Highest Available Upgrade: ", font, 10, 90, 14);
-
-		sf::Text* upgradeText = new sf::Text();
-		PopupWindows::makeText(upgradeText, this->techManager->getHighestAvaUpgrade()->getType(), font, 10, 130, 14);
-
 		while (techManagerInfo.isOpen()) {
 			 sf::Event ex;
 			 while (techManagerInfo.pollEvent(ex))
@@ -40,19 +27,10 @@ void TechManagerButton::onMouseClick(){
 
 			 background->paintBackground("Resources/carbon.jpg");
 
-			 techManagerInfo.draw(*level);
-			 techManagerInfo.draw(*levelText);
-			 techManagerInfo.draw(*upgrade);
-			 techManagerInfo.draw(*upgradeText);
+			 updateView(&techManagerInfo);
 
 			 techManagerInfo.display();
 		}
-
-		delete(level);
-		delete(levelText);
-		delete(upgrade);
-		delete(upgradeText);
-		delete(background);
 	}
 }
 
