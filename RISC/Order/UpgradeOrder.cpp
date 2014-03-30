198ad4573eaecd355acf7ac6c03689631c1095b3
Wei Chen <wei.zheng.chen@duke.edu>
2014-03-30 02:07:53 -0400

    updated update order

--- RISC/Order/UpgradeOrder.cpp
+++ RISC/Order/UpgradeOrder.cpp
@@ -7,16 +7,56 @@
 
 #include "UpgradeOrder.h"
 
-UpgradeOrder::UpgradeOrder(Unit* unit, UnitType* unitType) : Order () {
-	this->myUnit = unit;
+UpgradeOrder::UpgradeOrder(vector<Unit*> listOfUnitsToUpgrade, UnitType* unitType) : Order() {
+	this->listOfUnitsToUpgrade = listOfUnitsToUpgrade;
 	this->myUnitType = unitType;
+	this->unlocking = false;
+	this->unitUpgrading = false;
+	this->convertingUpgrade = false;
+}
+
+void UpgradeOrder::isUnlocking(bool unlocking){
+	this->unlocking = unlocking;
+}
+
+void UpgradeOrder::isUnitUpgrading(bool unitUpgrading){
+	this->unitUpgrading = unitUpgrading;
+}
+
+void UpgradeOrder::isConvertingUpgrade(bool convertingUpgrade){
+	this->convertingUpgrade = convertingUpgrade;
+}
+
+bool UpgradeOrder::getUnlocking(){
+	return this->unlocking;
+}
+
+bool UpgradeOrder::getUnitUpgrading(){
+	return this->unitUpgrading;
+}
+
+bool UpgradeOrder::getConvertingUpgrade(){
+	return this->convertingUpgrade;
 }
 
 Response* UpgradeOrder::execute(GameState* state) {
+	Response* returnResponse = new Response();
 
-	return nullptr;
+	if (this->convertingUpgrade){
+		//make in to a spy or turn back into a reg unit and modify return response
+	}
+
+	if (this->unitUpgrading){
+		//check the list of units to upgrade and modify return response
+	}
+
+	if (this->unlocking){
+
+		//unlock the next upgrade in the TechManager and modify return response
+	}
+
+	return returnResponse;
 }
-
 
 UpgradeOrder::~UpgradeOrder() {
 }
--- RISC/Order/UpgradeOrder.h
+++ RISC/Order/UpgradeOrder.h
@@ -7,11 +7,21 @@ using namespace std;
 
 class UpgradeOrder : public Order {
 public:
-	UpgradeOrder(Unit* unit, UnitType* unitType);
+	UpgradeOrder(vector<Unit*>, UnitType* unitType);
+	void isUnlocking(bool);
+	void isUnitUpgrading(bool);
+	void isConvertingUpgrade(bool);
+	bool getUnlocking();
+	bool getUnitUpgrading();
+	bool getConvertingUpgrade();
 	Response* execute(GameState* state);
 	~UpgradeOrder();
 
 private:
-	Unit* myUnit;
+	bool unlocking; //true /false is this to upgrade upgrades in the manager
+	bool unitUpgrading; // true/false this is to upgrade the units
+	bool convertingUpgrade; // to or false this is to convert and unit to a spy
+
+	vector<Unit*> listOfUnitsToUpgrade;
 	UnitType* myUnitType;
 };
