1b257a4ef301d5101f9b1664f72c595f09d8d7f8
CarlosReyesStoneham <cer26@duke.edu>
2014-04-13 14:17:04 -0400

    create order test writes stream to string and added serial string to vector

--- RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
+++ RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
 	Buffers::UnitType unitType;
 	
 	unitType.set_level(1);
@@ -19,9 +21,22 @@ int main(int argc, char* argv[]) {
 	unitType.set_costtounlock(5);
 	unitType.set_spy(false);
 	unitType.set_previoustype("tanks");
-	vector<Buffers::UnitType> orderList;
-	orderList.push_back(unitType);
+	
 
-	cout << "compiles!!" << endl;
+	string serialized_data;
+	{
+    // Write the new address book back to disk.
+    //fstream output(serialized_data, ios::out | ios::trunc | ios::binary);
+		if (!unitType.SerializeToString(&serialized_data)) {
+      		cerr << "Failed to write data stream." << endl;
+      		return -1;
+		} 
+	}
+	
+	vector<string> orderList;
+	orderList.push_back(serialized_data);
+
+	cout << "compiles!! "<< endl;
+	cout << orderList.at(0) << endl;
 
 }
