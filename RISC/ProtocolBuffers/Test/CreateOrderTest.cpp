ccf253a882978328c30e85e8170cd714bc0f7287
CarlosReyesStoneham <cer26@duke.edu>
2014-04-13 01:56:55 -0400

    added package Buffers and Test directory space

---
+++ RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../AddUnitOrder.pb.h"
+
+int main(int argc, char* argv[]) {
+
+	Buffers::AddUnitOrder* addUnitOrder = new AddUnitOrder();
+
+
+}
