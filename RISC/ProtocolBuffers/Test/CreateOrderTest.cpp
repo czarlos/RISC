d9a0035b83a06a891428b8bec3c3fbc2b42f56f3
CarlosReyesStoneham <cer26@duke.edu>
2014-04-13 14:57:26 -0400

    added outline for addunitorder serialization method and static unittype buffer creator

--- RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
+++ RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
@@ -40,3 +40,59 @@ int main(int argc, char* argv[]) {
 	cout << orderList.at(0) << endl;
 
 }
+
+/*Add Unit Order*/
+string serializeOrder() {
+	Buffers::AddUnitOrder addUnitOrder;
+	//destination
+	myTerritory->getTerritoryID();
+	myTerritory->getOwner();
+	myTerritory->getMaxResourceProduction();
+	myTerritory->getMaxCapacity();
+	myTerritory->getProduction(); //Vector
+	myTerritory->getLocation();
+	myTerritory->getContents();//Vector
+	myTerritory->isVisible();
+	
+	//unitList
+	for each (Unit* unit in myUnitList) {
+		//Make a Unit
+		//Add it
+		Buffers::Unit unitBuffer;
+		unitBuffer.set_health()
+		unitBuffer.set_unittype();
+		unitBuffer.set_teamname();
+		unitBuffer.set_unitid();
+		unitBuffer.set_movementrange();		
+
+
+		addUnitOrder.add_unitlist(unitBuffer);
+	}
+}
+
+Buffer::UnitType getUnitType(Unit*) {
+	string unitType = Unit->getUnitType->getType();
+	
+	Buffers::UnitType unitTypeBuffer;
+	
+	if (unitType == "Infantry") {
+		
+	}
+	else if (unitType == "Automatic Weapons") {
+	
+	}
+	else if (unitType == "Rocket Launchers") {
+	
+	}
+	else if (unitType == "Tanks") {
+	
+	}
+	else if (unitType == "Improved Tanks") {
+	
+	}
+	else if (unitType == "Fighter Planes") {
+	
+	}
+
+	return unitTypeBuffer;
+}
