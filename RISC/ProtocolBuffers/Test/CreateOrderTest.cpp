6e41f63ad6df71dc3ab029340eaedd7cd5518d69
CarlosReyesStoneham <cer26@duke.edu>
2014-04-13 13:53:23 -0400

    added makefile for compiling c++ protobuf code

--- RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
+++ RISC/ProtocolBuffers/Test/CreateOrderTest.cpp
@@ -1,28 +1,27 @@
+#include <google/protobuf/text_format.h>
 #include "stdlib.h"
 #include "stdio.h"
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "../AddUnitOrder.pb.h"
 #include "../UnitType.pb.h"
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-
-	//Buffers::AddUnitOrder* addUnitOrder = new AddUnitOrder();
-	//Buffers::Territory* territory = new Territory();	
-	//Buffers::Unit*
-	Buffers::UnitType* unitType;
+	Buffers::UnitType unitType;
 	
-	unitType->set_level(1);
-	unitType->set_type("infantry");
-	unitType->set_combatbonus(10);
-	unitType->set_upgradecost(2);
-	unitType->set_costtounlock(5);
-	unitType->set_spy(false);
-	unitType->set_previoustype("tanks");
+	unitType.set_level(1);
+	unitType.set_type("infantry");
+	unitType.set_combatbonus(10);
+	unitType.set_upgradecost(2);
+	unitType.set_costtounlock(5);
+	unitType.set_spy(false);
+	unitType.set_previoustype("tanks");
+	vector<Buffers::UnitType> orderList;
+	orderList.push_back(unitType);
+
 	cout << "compiles!!" << endl;
-	//addUnitOrder.set_allocated_destination(territory);
 
 }
