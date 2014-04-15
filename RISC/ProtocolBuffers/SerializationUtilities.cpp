b04f40ac7f6114c0cfd8ebc50f176cfa5d4cca34
CarlosReyesStoneham <cer26@duke.edu>
2014-04-15 11:00:04 -0400

    fixing serialization utilities and abstracting method

--- RISC/ProtocolBuffers/SerializationUtilities.cpp
+++ RISC/ProtocolBuffers/SerializationUtilities.cpp
@@ -5,78 +5,12 @@ SerializationUtilities::SerializationUtilities() {
 }
 
 Buffers::UnitType* SerializationUtilities::getUnitType(Unit* unit) {
-    
-	string unitType = unit->getUnitType()->getType();
-       
-    Buffers::UnitType* unitTypeBuffer = new Buffers::UnitType();
-       
-	if (unitType == "Infantry") {   
-	    unitTypeBuffer->set_level(0);
-    	unitTypeBuffer->set_type("Infantry");
-    	unitTypeBuffer->set_combatbonus(0);
-    	unitTypeBuffer->set_upgradecost(0);
-    	unitTypeBuffer->set_costtounlock(0);
-    	unitTypeBuffer->set_spy(false);
-		unitTypeBuffer->set_nexttype("AutomaticWeapons");
-		unitTypeBuffer->set_previoustype("");
-	}   
-	else if (unitType == "AutomaticWeapons") {
-        unitTypeBuffer->set_level(1);
-        unitTypeBuffer->set_type("AutomaticWeapons");
-        unitTypeBuffer->set_combatbonus(1);
-        unitTypeBuffer->set_upgradecost(3);
-        unitTypeBuffer->set_costtounlock(20);
-        unitTypeBuffer->set_spy(false);
-        unitTypeBuffer->set_nexttype("RocketLaunchers");
-        unitTypeBuffer->set_previoustype("Infantry"); 
-	}   
-	else if (unitType == "RocketLaunchers") {
-        unitTypeBuffer->set_level(2);
-        unitTypeBuffer->set_type("RocketLaunchers");
-        unitTypeBuffer->set_combatbonus(2);
-        unitTypeBuffer->set_upgradecost(19);
-        unitTypeBuffer->set_costtounlock(50);
-        unitTypeBuffer->set_spy(false);
-        unitTypeBuffer->set_nexttype("Tank");
-        unitTypeBuffer->set_previoustype("AutomaticWeapons"); 
-	}   
-	else if (unitType == "Tanks") {
-       	unitTypeBuffer->set_level(3);
-        unitTypeBuffer->set_type("Tanks");
-        unitTypeBuffer->set_combatbonus(6);
-        unitTypeBuffer->set_upgradecost(25);
-        unitTypeBuffer->set_costtounlock(80);
-        unitTypeBuffer->set_spy(false);
-        unitTypeBuffer->set_nexttype("Tanks");
-        unitTypeBuffer->set_previoustype("RocketLaunchers");    
-	}   
-	else if (unitType == "ImprovedTanks") {
-        unitTypeBuffer->set_level(4);
-        unitTypeBuffer->set_type("Infantry");
-        unitTypeBuffer->set_combatbonus(12);
-        unitTypeBuffer->set_upgradecost(25);
-        unitTypeBuffer->set_costtounlock(120);
-        unitTypeBuffer->set_spy(false);
-        unitTypeBuffer->set_nexttype("FighterPlanes");
-        unitTypeBuffer->set_previoustype("Tanks");
-	}   
-	else if (unitType == "FighterPlanes") {
-        unitTypeBuffer->set_level(5);
-        unitTypeBuffer->set_type("FighterPlanes");
-        unitTypeBuffer->set_combatbonus(15);
-        unitTypeBuffer->set_upgradecost(35);
-        unitTypeBuffer->set_costtounlock(150);
-        unitTypeBuffer->set_spy(false);
-        unitTypeBuffer->set_nexttype("");
-        unitTypeBuffer->set_previoustype("ImprovedTanks");
-	}   
-
-    return unitTypeBuffer;
+    return getUnitType(unit->getUnitType());
 }
 
-Buffers::UnitType* SerializationUtilities::getUnitType(UnitType* unitType) {
+Buffers::UnitType* SerializationUtilities::getUnitType(UnitType* unitT) {
     
-    string unitType = unitType->getType();
+    string unitType = unitT->getType();
        
     Buffers::UnitType* unitTypeBuffer = new Buffers::UnitType();
        
--- RISC/ProtocolBuffers/SerializationUtilities.h
+++ RISC/ProtocolBuffers/SerializationUtilities.h
@@ -4,9 +4,11 @@
 #include "Territory.pb.h"
 #include "UnitType.pb.h"
 #include "Unit.pb.h"
+#include "TechnologyManager.pb.h"
 #include "../GameObject/Unit/Unit.h"
 #include "../GameMap/Territory.h"
 #include "../GameMap/Location.h"
+#include "../GameState/Managers/TechnologyManager.h"
 
 class SerializationUtilities {
 
@@ -16,7 +18,7 @@ public:
 	static Buffers::UnitType* getUnitType(UnitType* unit);
 	static void createUnitBuffer(Unit* unit, Buffers::Unit* unitBuffer);
 	static Buffers::TechnologyManager* createTechnologyManagerBuffer(TechnologyManager* technology);
-	static void createUnitTypeBuffer(UnitType* unitType, Buffers::UnitType* unitTypeBuffer)
+	static void createUnitTypeBuffer(UnitType* unitType, Buffers::UnitType* unitTypeBuffer);
 	static Buffers::Territory* createTerritoryBuffer(Territory* myTerritory);
 	static Buffers::Location* createLocationBuffer(Location* location);
 	virtual ~SerializationUtilities();
